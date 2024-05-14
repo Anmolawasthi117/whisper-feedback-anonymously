3f046e5754114247c24018f99de0e922e8aaf49a
Anmol <anmolawasthi117@gmail.com>
2024-05-14 22:21:54 +0530

    console.log result

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -58,6 +58,7 @@ export default function SignInForm() {
 
     if (result?.url) {
       console.log('bhai sb shi chl rha hai')
+      console.log(result.url)
       router.replace('/dashboard');
     }
   };
