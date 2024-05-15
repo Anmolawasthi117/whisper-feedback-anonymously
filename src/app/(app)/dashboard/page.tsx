e84208c192d2e46e20f1d455a506bd374821c250
Anmol <anmolawasthi117@gmail.com>
2024-05-15 14:52:34 +0530

    logging dashboard page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -134,6 +134,8 @@ function UserDashboard() {
   };
 
   return (
+    <>
+    {console.log("dashboard page")}
     <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
       <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
 
@@ -191,6 +193,7 @@ function UserDashboard() {
         )}
       </div>
     </div>
+    </>
   );
 }
 
